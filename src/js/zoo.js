c6362385342ea377b3b7deb2786187e6b73aa36e
Syed Ali <sali3@cox.net>
2016-11-15 17:06:23 -0500

    Able to create function to getAge and giveBirth

--- src/js/zoo.js
+++ src/js/zoo.js
@@ -3,22 +3,60 @@
 
 console.log("I am in zoo");
 
-
+// Constructor function to create an Animal
 function Animal(name, dob){
-  this.name = name || "Fish";
-  this.dob = dob;
+  this.name = name || "Jimmy";
+  this.dob = dob || 20;
 }
+Animal.prototype.getAge = function getAge(){
+  console.log(this.dob);
+  // TODO: figure out how to calculate age in years
+  return 5;
+};
 
-console.log(new Animal("jack", 385621200000));
 
 
 
 
-// Animal.prototype.sayHello = function sayHello(){
-//   console.log("Hi my name is" + this.species);
-// };
-//
-// sayHello();
+
+// Constructor Function to create a Polar Bear which is species of Animal
+function PolarBear(name, dob) {
+  Animal.apply( this, [name, dob] );
+  this.species = "Polar Bear";
+}
+PolarBear.prototype = Object.create(Animal.prototype);
+PolarBear.prototype.constructor = PolarBear;
+
+
+console.log(new PolarBear("Matt", 40));
+
+
+// Constructor Function to create a Arctic Fox which is species of Animal
+function ArcticFox(name, dob) {
+  Animal.apply( this, [name, dob] );
+  this.species = "Arctic Fox";
+}
+ArcticFox.prototype = Object.create(Animal.prototype);
+ArcticFox.prototype.constructor = ArcticFox;
+
+
+
+
+
+var momFox = new ArcticFox("Jamie", 28);
+
+
+
+ArcticFox.prototype.giveBirth = function giveBirth(){
+  return new ArcticFox("Saddam", 1);
+};
+
+console.log(momFox.giveBirth());
+
+
+
+
+
 
 
 
