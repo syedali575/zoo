080dd2c02df74208f6695d71eea74b92964602c2
Syed Ali <sali3@cox.net>
2016-11-15 14:21:57 -0500

    Created animal contructor function

--- src/js/zoo.js
+++ src/js/zoo.js
@@ -4,10 +4,21 @@
 console.log("I am in zoo");
 
 
+function Animal(name, dob){
+  this.name = name || "Fish";
+  this.dob = dob;
+}
+
+console.log(new Animal("jack", 385621200000));
 
 
 
 
+// Animal.prototype.sayHello = function sayHello(){
+//   console.log("Hi my name is" + this.species);
+// };
+//
+// sayHello();
 
 
 
